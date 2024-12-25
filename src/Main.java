bf6860b3793a40861eeeeaa9bae3acdcabcf147b
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:32:23 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Book book1 = new AdventureBook("Vefxistyaosani", 300, "Shota Rustaveli", true);
+        Book book2 = new ScienceBook("Quantum Mechanics", 200, "Jane Smith", false, "Physics");
+        Book book3 = new FantasyBook("The Dragon's Tale", 450, "Mark Brown", true, "Greece");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Employee employee1 = new BookArranger("Alice", "Johnson", 1);
+        Employee employee2 = new BookIssuer("Bob", "Williams", 2, "09:00", "17:00");
+        Employee employee3 = new BookAdvisor("Charlie", "Davis", 3, 4.5, "Public Library");
+        Employee employee4 = new SilenceController("David", "Miller", 4, 8.0);
+
+
+        Employee[] employees = {employee1, employee2, employee3, employee4};
+        Book[] books = {book1, book2, book3};
+        Library library = new Library("City Library", employees, books);
+
+
+        System.out.println(library);
+
+        BookAdvisor advisor = (BookAdvisor) employee3;
+        String[] bookTitles = {"The Lost City", "Quantum Mechanics", "The Dragon's Tale"};
+        System.out.println("Recommended Book: " + advisor.recommendBook(bookTitles));
     }
 }

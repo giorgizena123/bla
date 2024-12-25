7f1bc4a43ee72a1996bfc9cdb4bc3b32b290adb8
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:30:30 +0400

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+class Book {
+    protected String title;
+    protected int pages;
+    protected String author;
+    protected boolean hardCover;
 
-public class Book {
+    public Book(String title, int pages, String author, boolean hardCover) {
+        this.title = title;
+        this.pages = pages;
+        this.author = author;
+        this.hardCover = hardCover;
+    }
+
+    public void read() {
+        System.out.println("I am reading the book " + title);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", pages=" + pages +
+                ", author='" + author + '\'' +
+                ", hardCover=" + hardCover +
+                '}';
+    }
 }

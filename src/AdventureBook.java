9102aa9e79a1e019fdb32db064c41a30acf2eff9
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:31:13 +0400

    Update AdventureBook.java

--- src/AdventureBook.java
+++ src/AdventureBook.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+class AdventureBook extends Book {
+    public AdventureBook(String title, int pages, String author, boolean hardCover) {
+        super(title, pages, author, hardCover);
+    }
 
-public class AdventureBook {
+    @Override
+    public String toString() {
+        return "AdventureBook{" + super.toString() + "}";
+    }
+}
+
+class ScienceBook extends Book {
+    private String scienceField;
+
+    public ScienceBook(String title, int pages, String author, boolean hardCover, String scienceField) {
+        super(title, pages, author, hardCover);
+        this.scienceField = scienceField;
+    }
+
+    @Override
+    public String toString() {
+        return "ScienceBook{" +
+                super.toString() +
+                ", scienceField='" + scienceField + '\'' +
+                '}';
+    }
+}
+
+class FantasyBook extends Book {
+    private String country;
+
+    public FantasyBook(String title, int pages, String author, boolean hardCover, String country) {
+        super(title, pages, author, hardCover);
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "FantasyBook{" +
+                super.toString() +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }

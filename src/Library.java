877f1c3bb40d044353084ec1a273ca7265fa83d5
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:31:58 +0400

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+class Library {
+    private String name;
+    private Employee[] employees;
+    private Book[] books;
 
-public class Library {
+    public Library(String name, Employee[] employees, Book[] books) {
+        this.name = name;
+        this.employees = employees;
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder employeeInfo = new StringBuilder();
+        for (Employee e : employees) {
+            employeeInfo.append(e.toString()).append("\n");
+        }
+
+        StringBuilder bookInfo = new StringBuilder();
+        for (Book b : books) {
+            bookInfo.append(b.toString()).append("\n");
+        }
+
+        return "Library{" +
+                "name='" + name + '\'' +
+                ", employees=\n" + employeeInfo.toString() +
+                ", books=\n" + bookInfo.toString() +
+                '}';
+    }
 }
